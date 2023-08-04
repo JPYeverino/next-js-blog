67e0781e9d59d1cb63bb6523045f3187a1e38ca4
Pablo  Yeverino <pabloyeverino@Pablos-MBP.attlocal.net>
2023-08-04 10:38:23 -0500

    Add i18n config and messages

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "public/hero.png";
+import { useTranslations } from "next-intl";
+import { MESSAGES } from "./messages";
 
 export default function Home() {
+  const t = useTranslations();
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <h1 className={styles.title}>
-          Better design for your digital products.
-        </h1>
+        <h1 className={styles.title}>{t(MESSAGES.TITLE)}</h1>
         <p className={styles.desc}>
           Turning your idea into Reality. We bring together the teams from the
           global tech industry
